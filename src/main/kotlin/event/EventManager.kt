a1d773208fcab331eae73e46b9cb966acfecbbbf
Momel <149087546+Momeliaa@users.noreply.github.com>
2023-11-25 22:14:54 +0900

    Add files via upload

--- src/main/kotlin/event/EventManager.kt
+++ src/main/kotlin/event/EventManager.kt
@@ -1,14 +1,21 @@
 package event
 
 import calendar.Date
+import java.io.*
 
-class Event(private var startDatetime: Date, private var endDatetime: Date, private var contents: String)
+class Event(private var title: String, private var startDatetime: Date, private var endDatetime: Date, private var contents: String)
 {
+    public fun getTitle():String {
+        return title
+    }
     public fun getStartDate():Date {
         return startDatetime
     }
     public fun getEndDate():Date {
         return endDatetime
+    }
+    public fun getContents():String {
+        return contents
     }
     private fun compareTimeGap(dt_start: Date, dt_end: Date): Boolean {
         val result=dt_start.getData().compareTo(dt_end.getData())
@@ -18,12 +25,26 @@ class Event(private var startDatetime: Date, private var endDatetime: Date, priv
         return true
     }
 }
+
 object EventManager {
     var eventList: ArrayList<Event> = ArrayList()
     val file: String="File.txt"
 
     fun fileLink():Unit{
-        //파일 연동
+        val printWriter = PrintWriter(file)
+
+        for (x in eventList) {
+            val title = x.getTitle()
+            val startDate = x.getStartDate().getData()
+            val startTime = x.getStartDate().getTime()
+            val endDate = x.getEndDate().getData()
+            val endTime = x.getEndDate().getTime()
+            val contents = x.getContents()
+
+            printWriter.println("$title $startDate $startTime ~ $endDate $endTime $contents")
+        }
+        printWriter.close()
+        println("이벤트 저장 완료")
     }
 
     fun insertEvent(event:Event):Unit {
@@ -40,4 +61,5 @@ object EventManager {
         }
         return tempList
     }
+
 }
